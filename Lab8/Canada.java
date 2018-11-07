66f043dddeb5042a623f2f540ddb90f3b9f1d8cd
Unknown <monika.silvia.s@hotmail.ca>
2018-11-06 22:05:18 -0800

    updated code for first method and tried working on next method

--- Lab8/Canada.java
+++ Lab8/Canada.java
@@ -8,30 +8,30 @@
 public class Canada
 {
     // instance variables - replace the example below with your own
-        public static final int BC = 0;
-        public static final int AB = 1;
-        public static final int MB = 2;
-        public static final int NB = 3;
-        public static final int NL = 4;
-        public static final int NT = 5;
-        public static final int NS = 6;
-        public static final int NU = 7;
-        public static final int ON = 8;
-        public static final int PE = 9;
-        public static final int QC = 10;
-        public static final int SK = 11;
-        public static final int YT = 12;
-        
-        private ProvinceTerritory[] provinces;
-      
+    public static final int BC = 0;
+    public static final int AB = 1;
+    public static final int MB = 2;
+    public static final int NB = 3;
+    public static final int NL = 4;
+    public static final int NT = 5;
+    public static final int NS = 6;
+    public static final int NU = 7;
+    public static final int ON = 8;
+    public static final int PE = 9;
+    public static final int QC = 10;
+    public static final int SK = 11;
+    public static final int YT = 12;
+
+    private ProvinceTerritory[] provinces;
+
     /**
      * Constructor for objects of class Canada
      */
     public Canada()
     {
-        
+
         provinces = new ProvinceTerritory[13]; 
-        
+
         provinces[BC]=new ProvinceTerritory("British Columbia",4400057);
         provinces[AB]=new ProvinceTerritory("Alberta",3645257);
         provinces[MB]=new ProvinceTerritory("Manitoba",1208268);
@@ -45,8 +45,7 @@ public class Canada
         provinces[QC]=new ProvinceTerritory("Quebec",7903001);
         provinces[SK]=new ProvinceTerritory("Saskatchewan",1033381);
         provinces[YT]=new ProvinceTerritory("Yukon",33897);
-        
-        
+
     
     }
 
@@ -60,15 +59,15 @@ public class Canada
         // put your code here
         int index = 0;
         int sum = 0;
-        
+
         while(index<=provinces.length-1){
             sum+=provinces[index].getPopulation();
             index+=1;
         }
-        
+
         return sum;
     }
-    
+
     /**
      * Method
      * 
@@ -80,20 +79,19 @@ public class Canada
         int     index = 0;
         String  lowProvinceName = "";
         int     lowProvincePopulation = 13000000;
-        
-        
+
         while(index<=provinces.length-1){
             if(provinces[index].getPopulation()<lowProvincePopulation){
-               lowProvinceName =  provinces[index].getName();
-               lowProvincePopulation =  provinces[index].getPopulation();
+                lowProvinceName =  provinces[index].getName();
+                lowProvincePopulation =  provinces[index].getPopulation();
             }
 
             index+=1;
         }
-        
+
         return lowProvinceName;
     }
-    
+
     /**
      * Method
      * 
@@ -104,86 +102,115 @@ public class Canada
         // put your code here
         int     NO_SUCH_PROVINCE = -1;
         int     index = 0;
-        
+
         if(province != null && !province.equals("")){
 
             while(index<=provinces.length-1){
                 if(provinces[index].getName().equals(province)){
-                   return provinces[index].getPopulation();
+                    return provinces[index].getPopulation();
                 }
-                   index+=1;
+                index+=1;
             }
-            
+
             return NO_SUCH_PROVINCE;
         }
         else {
             throw new IllegalArgumentException("Cannot be null or empty");
         }
     }
-    
+
     public String[] getProvincesWithPopulationBetween(int min, int max){
         int i                   = 0;
         int j                   = 0;
         int numOfProvWithPop    = 0;
         String[] matchingProvinces;
-        
+
         while(i<provinces.length){
             if((provinces[i].getPopulation()>=min)&&(provinces[i].getPopulation()<=max)){
                 numOfProvWithPop++;
             }
             i++;
         }
-        
+
         if(numOfProvWithPop>0){
-            matchingProvinces = new ProvinceTerritory[numOfProvWithPop];
+            matchingProvinces = new String[numOfProvWithPop];
         } else {
             //no matches
             return null;
         }
-        
+
         i=0; //start lookin from the beginning again
         while(i<provinces.length){
             if((provinces[i].getPopulation()>=min)&&(provinces[i].getPopulation()<=max)){
-                matchingProvinces[j]=provinces[i];
+                matchingProvinces[j]=provinces[i].getName();
                 j++;
             }
             i++;
         }
-        
+
         return matchingProvinces;
     }
-    
+
     public boolean isProvinceInCanada(String name){
         int     index = 0;
-        
+
         if(name != null && !name.equals("")){
             while(index<=provinces.length-1){
                 if(provinces[index].getName().equals(name)){
-                   return true;
+                    return true;
                 }
-                   index+=1;
+                index+=1;
             }
-            
+
             return false;
         }else{
             throw new IllegalArgumentException("The province is not valid.");
         }
     }
-    
+
     public String[] getProvincesWhoseNameContains(String substring){
+        int index = 0;
+        String[] matchingProvinces;
+
         if(substring != null && !substring.equals("")){
-            
+            if(provinces.contains(substring)){
+                return provinces[0].getName();
+            }
+            else {
+                throw new IllegalArgumentException("The name of the province is not valid.");
+            }
+        }
+    }
+        /**
+        public ProvinceTerritory[] getMoreProvincesWhoseNameContains(String substring){
+        if(substring != null && !substring.equals("")){
+
         }
         else {
-            throw new IllegalArgumentException("The name of the province is not valid.");
+        throw new IllegalArgumentException("The name of the province is not valid.");
         }
+        }
+         */
+        /**
+        public String[] getProvincesWhoseNameStartsWith(char letter){
+        String[] matchingProvinces;
+        int     index = 0;
+
+        if(substring != null && !substring.equals("")){
+
+        while(index<=provinces.length-1){
+        if(provinces[0].getName().charAt(0).equals(letter)){
+        return matchingProvinces; 
+        }
+        index+=1;
+        }
+        return false 
+        }
+        else {
+        throw new IllegalArgumentException("The name of the province is not valid.");
+        }
+
+        
+        }
+         **/
     }
-    
-    public ProvinceTerritory[] getMoreProvincesWhoseNameContains(String substring){
-    
-    }
-    
-    public String[] getProvincesWhoseNameStartsWith(char letter){
-    
-    }
-}
